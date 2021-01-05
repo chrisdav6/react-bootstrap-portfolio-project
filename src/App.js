8f1e07a2b00b0b5bae5417cee37bce11ed2d465c
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2021-01-05 13:49:51 -0500

    Create and style AboutMe component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Particles from 'react-particles-js';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
+import AboutMe from './components/AboutMe';
 
 const App = () => {
   return (
@@ -28,6 +29,7 @@ const App = () => {
       />
       <Navbar />
       <Header />
+      <AboutMe />
     </div>
   );
 };
