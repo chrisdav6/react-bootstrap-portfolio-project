95d330d5530c0652c55fe2f7d308d848b5d61c4a
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2021-01-05 13:22:31 -0500

    Create and style Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import Particles from 'react-particles-js';
 import Navbar from './components/Navbar';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <div className='app'>
+      <Particles
+        className='particles-canvas'
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            },
+            shape: {
+              type: 'circle',
+              stroke: {
+                width: 6,
+                color: '#f9ab00'
+              }
+            }
+          }
+        }}
+      />
       <Navbar />
-      <h2>Portfolio Project</h2>
+      <Header />
     </div>
   );
 };
---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Typed from 'react-typed';
+
+const Header = () => {
+  return (
+    <div className='header-wrapper'>
+      <div className='main-info'>
+        <h1>Web Development and Website Promotions</h1>
+        <Typed
+          className='typed-text'
+          strings={[
+            'Web Development',
+            'Web Design',
+            'SocialMedia',
+            'Google Analytics'
+          ]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        />
+        <a href='#' className='btn-main-offer'>
+          Contact Me
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
